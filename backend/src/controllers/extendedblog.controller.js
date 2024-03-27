4486f952a506999359030ec153a5937a236e43c0
Anderson Martinez <91474793+anderman1981@users.noreply.github.com>
2024-03-27 12:52:27 -0700

    Update extendedblog.controller.js

--- backend/src/controllers/extendedblog.controller.js
+++ backend/src/controllers/extendedblog.controller.js
@@ -1,17 +1,34 @@
 import { getConnection } from "../database/database.js";
+import redis from "ioredis";
+
+const redisClient = new redis();
 
 const getExtendedBlogs = async( req, res ) => {
     try{
+        const cachedData = await redisClient.get("extendedblog");
+        if (cachedData) {
+            const hasChanges = await checkForChanges();
+            if (!hasChanges) {
+                console.log("Sirviendo destinos de la caché");
+                return res.json(JSON.parse(cachedData));
+            }
+        }
         const connection = await getConnection();
         const result = await connection.query("SELECT * FROM extended_blog;");
-        console.log(result[0]);
+        
+        await redisClient.set("extendedblog", JSON.stringify(result[0]));
+    
         res.json(result[0]);
     }catch (error) {
-        res.status(500);
-        res.send(error.message);
+        console.error("Error al obtener destinos:", error);
+        res.status(500).send("Error interno del servidor");
     }
 };
 
+const checkForChanges = async () => {
+    return false; 
+};
+  
 export const methods = {
-    getExtendedBlogs
+    getExtendedBlogs,
 };
